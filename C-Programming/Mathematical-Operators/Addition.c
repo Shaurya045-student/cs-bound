15f402d132f7b03670330d6e5ccf176f4ea2ac0a
Shaurya045-student <vshaurya657@gmail.com>
2026-07-20 23:24:35 +0530

    Addition Operator

---
+++ C-Programming/Mathematical-Operators/Addition.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ int main() {
+    int num1 , num2;
+    printf("Enter Number 1 : ");
+    scanf("%d", &num1);
+    printf("Enter Number 2 : ");
+    scanf("%d", &num2);
+    printf("Sum of %d and %d is : %d", num1 , num2 , num1+num2);
+    return 0;
+ }
