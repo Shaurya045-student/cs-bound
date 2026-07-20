4fdb838349c782136fddbc46df55c01d0b55717c
Shaurya045-student <vshaurya657@gmail.com>
2026-07-20 22:50:54 +0530

    Multiplication Operator

---
+++ C-Programming/Mathematical-Operators/Multiply.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    printf("Enter Number 1 : ");
+    scanf("%d", &num1);
+    printf("Enter Number 2 : ");
+    scanf("%d", &num2);
+    printf("Multiplication of %d and %d is : %d", num1, num2, num1 * num2);
+    return 0;
+}
